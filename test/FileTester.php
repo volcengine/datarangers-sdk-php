ae5169dd92ceefc3fd521ba1b4aa318bc81f1003
hezhiwei.alden@bytedance.com <hezhiwei.alden@bytedance.com>
2021-01-14 11:19:47 +0800

    [update]item support

--- test/FileTester.php
+++ test/FileTester.php
@@ -13,14 +13,16 @@ CollectorConfig::init_datarangers_collector([
 ]);
 $rc = new AppEventCollector(new FileConsumer());
 //example01: send multiple event
-$rc->sendEvent("uuid16981", 10000002, null, ["__profile_set", "php_event"],
-    [["php_name" => "php", "php_version" => "5.6","float_param"=>floatval(5)], ["php_name" => "php", "php_version" => "5.6", "items" => [
-        ["item_id" => "0001", "item_name" => "book"], ["item_id" => "0002", "item_name" => "book"], ["item_id" => "0001", "item_name" => "phone"]
-    ]]]);
-$rc->sendEvent("uuid16981", 10000002, null, "php_event",
-    ["php_name" => "php", "php_version" => "5.6", "float_param" => floatval(5)]);
-$rc->itemUnset(10000002, "book", "0001", ["param1", "param2"]);
+for ($i = 0; $i < 1000; $i++) {
+    $rc->sendEvent("uuid16981", 10000002, null, ["__profile_set", "php_event"],
+        [["php_name" => "php", "php_version" => "5.6", "float_param" => floatval(5)], ["php_name" => "php", "php_version" => "5.6", "items" => [
+            ["item_id" => "0001", "item_name" => "book"], ["item_id" => "0002", "item_name" => "book"], ["item_id" => "0001", "item_name" => "phone"]
+        ]]]);
+    $rc->sendEvent("uuid16981", 10000002, null, "php_event",
+        ["php_name" => "php", "php_version" => "5.6", "float_param" => floatval(5)]);
+    $rc->itemUnset(10000002, "book", "0001", ["param1", "param2"]);
 //example02: send item set event
-$rc->itemSet(10000002, ["item_id" => "0002", "item_name" => "book", "name" => "name", "price" => 1.2]);
+    $rc->itemSet(10000002,"book", ["item_id" => "0002", "item_name" => "book", "name" => "name", "price" => 1.2]);
 //example03: send profile set event
-$rc->profileSet("uuid16981", 10000002,["php_name" => "php", "php_version" => "5.6","float_param"=>floatval(5)]);
+    $rc->profileSet("uuid16981", 10000002, ["php_name" => "php", "php_version" => "5.6", "float_param" => floatval(5)]);
+}
